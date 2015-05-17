df7e06c0ef2c1b1b6cb42fe821db68a0334b0317
Leonardo Suzan <leonardosuzan@minutostelecom.com.br>
2015-05-17 19:01:55 -0300

    Carregando arquivo CSV de contato para objeto em memória.

---
+++ src/main/java/dialer/ContatoFieldSetMapper.java
@@ -0,0 +1,31 @@
+package dialer;
+
+import org.springframework.batch.item.file.mapping.*;
+import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.validation.BindException;
+
+public class ContatoFieldSetMapper implements FieldSetMapper<Contato> {
+
+	@Override
+	public Contato mapFieldSet(FieldSet arg0) throws BindException {
+		
+		Contato contato = new Contato();
+		
+		contato.setPrimeiroNome(arg0.readString(0));
+		contato.setSobrenome(arg0.readString(1));
+		contato.setCodigoArea(arg0.readString(2));
+		contato.setNumeroTelefonico(arg0.readString(3));
+		contato.setEndereco(arg0.readString(4));
+		contato.setNumeroEndereco(arg0.readString(5));
+		contato.setComplemento(arg0.readString(6));
+		contato.setBairro(arg0.readString(7));
+		contato.setCidade(arg0.readString(8));
+		contato.setEstado(arg0.readString(9));
+		contato.setObservacoes(arg0.readString(10));
+		
+		
+		
+		return contato;
+	}
+	
+}
--- src/main/java/dialer/FileUploadController.java
+++ src/main/java/dialer/FileUploadController.java
@@ -5,6 +5,12 @@ import java.io.File;
 import java.io.FileOutputStream;
 
 import org.apache.commons.io.FilenameUtils;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
@@ -35,7 +40,7 @@ public class FileUploadController {
             @RequestParam("file") MultipartFile file,
             Model model,
             SessionStatus status,
-            @ModelAttribute("campaign") Campaign campaign) {
+            @ModelAttribute("campaign") Campaign campaign) throws Exception {
     	
     	model.addAttribute("campaign", campaign);
     	
@@ -60,9 +65,33 @@ public class FileUploadController {
                 stream.write(bytes);
                 stream.close();
                 model.addAttribute("status","You successfully uploaded " + fileName + "!");
+                
+                
+                //Começa leitura do arquivo
+                FlatFileItemReader<Contato> itemReader = new FlatFileItemReader<Contato>(); 
+                itemReader.setResource(new FileSystemResource(fileName));
+                //DelimitedLineTokenizer defaults to comma as its delimiter 
+                LineMapper<Contato> lineMapper = new DefaultLineMapper<Contato>(); 
+                ((DefaultLineMapper<Contato>) lineMapper).setLineTokenizer(new DelimitedLineTokenizer()); 
+                ((DefaultLineMapper<Contato>) lineMapper).setFieldSetMapper(new ContatoFieldSetMapper()); 
+                itemReader.setLineMapper(lineMapper);
+                itemReader.open(new ExecutionContext()); 
+
+                Contato contato = itemReader.read();
+                contato = itemReader.read();
+                
+                contato.toString();
+                
+                
+                
                 return "showCSV";
+           
+            
             } catch (Exception e) {
+            	//throw e;
+            	e.printStackTrace();
             	model.addAttribute("status","You failed to upload " + fileName + " => " + e.getMessage());
+            	
                 return "importList";
             }
         } else {
